3cfd2805c0cd0bfc3ac0682d701691486741a2f3
JacobAZimmerman <ijacobzim@gmail.com>
2018-05-25 08:53:18 -0700

    functionality for get and delete

--- index.js
+++ index.js
@@ -8,6 +8,51 @@ const { PORT, CLIENT_ORIGIN } = require('./config');
 const { dbConnect } = require('./db-mongoose');
 // const {dbConnect} = require('./db-knex');
 
+let Queue = require('./queue.js');
+
+let cats = new Queue();
+let dogs = new Queue();
+
+cats.enqueue({
+  imageURL:'https://assets3.thrillist.com/v1/image/2622128/size/tmg-slideshow_l.jpg', 
+  imageDescription: 'Orange bengal cat with black stripes lounging on concrete.',
+  name: 'Fluffy',
+  sex: 'Female',
+  age: 2,
+  breed: 'Bengal',
+  story: 'Thrown on the street'
+});
+
+cats.enqueue({
+  imageURL:'https://www.thehappycatsite.com/wp-content/uploads/2016/12/calico-cat-2.jpg', 
+  imageDescription: 'Calico cat looking for her next owner',
+  name: 'Mittens',
+  sex: 'Female',
+  age: 1,
+  breed: 'Calico',
+  story: 'Owner left her behind for a new apartment'
+});
+
+dogs.enqueue({
+  imageURL: 'http://www.dogster.com/wp-content/uploads/2015/05/Cute%20dog%20listening%20to%20music%201_1.jpg',
+  imageDescription: 'A smiling golden-brown golden retreiver listening to music.',
+  name: 'Zeus',
+  sex: 'Male',
+  age: 3,
+  breed: 'Golden Retriever',
+  story: 'Owner Passed away'
+});
+
+dogs.enqueue({
+  imageURL: 'https://canna-pet.com/wp-content/uploads/2017/10/german-shepherd-dog-1071592_1920-1024x683.jpg',
+  imageDescription: 'A cute German Shepherd lounging in the grass',
+  name: 'Maia',
+  sex: 'Female',
+  age: 1,
+  breed: 'German Shepherd',
+  story: 'Found wandering around with no owner'
+});
+
 const app = express();
 
 app.use(
@@ -21,6 +66,22 @@ app.use(
     origin: CLIENT_ORIGIN
   })
 );
+
+app.get('/api/cat', function(req, res, err) {
+  res.json(cats.peek());
+});
+
+app.get('/api/dog', function(req, res, err) {
+  res.json(dogs.peek());
+});
+
+app.delete('/api/cat/', function(req, res, err) {
+  res.json(cats.dequeue());
+});
+
+app.delete('/api/dog/', function(req, res, err) {
+  res.json(dogs.dequeue());
+});
 
 function runServer(port = PORT) {
   const server = app
